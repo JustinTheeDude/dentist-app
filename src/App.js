5271754c8187952371082f16a5ecab5a98f13174
Omar Gonzalez <gonzalezoe82@gmail.com>
2020-01-21 14:54:07 +0900

    implemented a minimum selected date

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
-import React, {Component} from "react";
-import Header from "./Components/Header";
+import React, { Component } from "react";
 import MainInfo from "./Components/MainInfo";
 import Calendar from "react-calendar";
 import DeliveryDate from "./Components/DeliveryDate";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+import Header from './Components/Header';
+
 
 class App extends Component {
 
     state = {
-        date: new Date(),
-      }
-    
-    onChange = date => this.setState({ date })
-   
-    render() {
+        startDate: new Date(),
+        deliveryDate: "",
+        minDate: new Date(),
+        month: ""
+    };
 
+    onChange = deliveryDate => {
+        this.setState({deliveryDate});
+        // console.log(this.state.deliveryDate);
+    };
+    
+
+    render() {
         return (
             <div>
                 <Header />
                 <MainInfo />
                 <h3 className="order-heading">発注日/納期日</h3>
                 <Calendar
-                selectRange={true}  
-                calendarType="US"
-                // activeStartDate={today => console.log(today)}
-                onClickDay={ value => console.log(value.toString().slice(0,16))  } 
+                    calendarType="US"
+                    onClickDay={this.onChange}
+                    minDate={  this.state.minDate}
+                    onClickMonth={this.showMonth}
                 />
-                 <DeliveryDate day={this.state.date.toString().slice(0,16)}/>
+                <DeliveryDate
+                    startDate={this.state.startDate.toString().slice(0, 16)}
+                    delivery={this.state.deliveryDate.toString().slice(0, 16)}
+                />
             </div>
         );
     }
-
 }
 
 export default App;
 
-// this.value[0].toString().slice(0,16) this.value[1].toString().slice(0,16)
+
