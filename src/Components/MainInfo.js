240c042a19a5e4fd8dade304bcd322f437166184
Omar Gonzalez <gonzalezoe82@gmail.com>
2020-01-23 14:13:37 +0900

    removed redundant input fields

--- src/Components/MainInfo.js
+++ src/Components/MainInfo.js
@@ -39,18 +39,6 @@ class MainInfo extends Component {
                         <Input type="text" name="text" placeholder="名前" />
                     </Col>
                 </FormGroup>
-                <FormGroup row>
-                    {/* This should be removed the order date should be set with the calendar */}
-                    <Label>発注日</Label>
-                    <Col sm={10}>
-                        <div className="order-div  year-div">年:</div>
-                        <div className="order-div month-div">月:</div>
-                        <div className="order-div day-div">日:</div>
-                        <Input type="text" name="text" placeholder="年" required />
-                        <Input type="text" name="text" placeholder="月" required />
-                        <Input type="text" name="text" placeholder="日" required />
-                    </Col>
-                </FormGroup>
                 <h3 className="patient-info-header">患者情報</h3>
                 <FormGroup row>
                     <Label>患者名</Label>
@@ -81,24 +69,6 @@ class MainInfo extends Component {
                         <option id="option-other">他</option>
                     </Input>
                     {this.state.value === "他" && <OtherOption />}
-                </FormGroup>
-                <FormGroup row>
-                    {/* This should also be removed the delivery date should be set with the calendar */}
-                    <Label>納期日</Label>
-                    <Col sm={10}>
-                        <div>
-                            月:
-                            <Input type="text" name="text" placeholder="月" required />
-                            日:
-                            <Input type="text" name="text" placeholder="日" required />
-                        </div>
-                        <div>
-                            時:
-                            <Input type="text" name="text" placeholder="時" required />
-                            分:
-                            <Input type="text" name="text" placeholder="分" required />
-                        </div>
-                    </Col>
                 </FormGroup>
                 <FormGroup>
                     <Label for="exampleSelect">支払い</Label>
